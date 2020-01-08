9eef91279c6d00b40b2dee0d401425d46c5efc96
Student <example@example.com>
2020-01-08 11:35:47 -0800

    add stylings and the beginings of app

--- app.rb
+++ app.rb
@@ -9,4 +9,29 @@ DB = PG.connect({:dbname => 'theatrix'})
 also_reload 'lib/**/*.rb'
 
 get '/' do
+    @page = 'Home'
+    @css = 'home'
+    erb :index
+end
+
+get '/theatres' do
+    @oage = 'Theatres'
+    @css = 'theatres'
+    @theatres = Theatre.all
+    erb :theatres
+end
+
+get '/theatres/new' do
+    @oage = 'New Theatre'
+    @css = 'New'
+    erb :theatres_new
+end
+post '/theatres' do
+    Theatre.new(params).save
+    redirect to '/theatres'
+end
+
+get '/theatres/:id' do
+    @theatre = Theatre.find(params[:id].to_i)
+    erb :theatres_ID
 end
